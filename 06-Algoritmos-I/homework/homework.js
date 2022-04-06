3ca2ef8ea5fa9ca888a10759b1ce315c8f88a6d4
unknown <1803684g@umich.mx>
2022-04-05 19:41:25 -0500

    AlgortmosI

--- 06-Algoritmos-I/homework/homework.js
+++ 06-Algoritmos-I/homework/homework.js
@@ -1,11 +1,33 @@
 'use strict'
 // No cambies los nombres de las funciones.
 
+function esPrimo(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
 function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let array = [];
+  let divisor = 2;
+  while (num != 1) {
+    if (esPrimo(divisor) && num % divisor == 0) {
+      num = num / divisor;
+      array.push(divisor);
+      divisor = 2
+    } else {
+      divisor++;
+    }
+  }
+  array.unshift(1);
+  return array;
 
 }
 
@@ -15,6 +37,18 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j + 1] < array[j]) {
+        let aux = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = aux;
+      }
+    }
+  }
+
+  return array;
+
 }
 
 
@@ -23,6 +57,18 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  let key = 0;
+  for (let i = 1; i < array.length; i++) {
+    key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = key;
+  }
+
+  return array;
 
 }
 
@@ -32,6 +78,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    let minimo = i;
+    for (let j = i; j < array.length; j++) {
+      if (array[minimo] > array[j]) {
+        minimo = j;
+      }
+    }
+
+    let aux = array[i];
+    array[i] = array[minimo];
+    array[minimo] = aux;
+  }
+
+  return array;
 
 }
 
